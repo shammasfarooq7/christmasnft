b952fb3d3161791ee112d32d85eeb8682b912f0b
shammasfarooq7 <shammasfarooq7@gmail.com>
2023-12-03 02:28:07 +0500

    App js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom"
+import LandingPage from "./pages/landing-page";
 
 function App() {
+  const pathname = window.location.pathname;
+
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/modal":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/single-product-page":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/thank-you-page":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/landing-page":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/landing-page" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
